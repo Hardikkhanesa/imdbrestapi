c4a45414b78351be56205edc59ccfb4294580600
Hardikkhanesa <hkhanesa@gmail.com>
2019-11-05 16:15:52 +0530

    added database capability

--- task.py
+++ task.py
@@ -2,57 +2,91 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import sqlite3
+import itertools
+from datetime import datetime
+import time
+try:
+    conn = sqlite3.connect('imdb.db')
+except Error as e:
+    print(e)
 
 # Download IMDB's Top 250 data
 url = 'https://www.imdb.com/chart/top?ref_=nv_mv_250'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-
+bad_chars = [';', ':', '!', "*","\n","[","]","(",")"]
 movies = soup.select('td.titleColumn')
 
 links = [a.attrs.get('href') for a in soup.select('td.titleColumn a')]
 #print(links)
-crew = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
+movietitle = [a.string for a in soup.select('td.titleColumn a')]
+
 ratings = [b.attrs.get('data-value') for b in soup.select('td.posterColumn span[name=ir]')]
-votes = [b.attrs.get('data-value') for b in soup.select('td.ratingColumn strong')]
+#print(ratings)
 
+
+
+newlist=[]
+#for i in range(250):
+#    newdata=[movietitle[i],ratings[i]]
+#    newlist.append(newdata)
+#print(newlist[0][0])
+#for i in range(250):
+#    cur.execute(sql,newlist[i])
 imdb = []
-
+mylist=[]
+rd=[]
+dur=[]
+desc=[]
+c=0
 for link in links:
     newurl = 'https://www.imdb.com'+link
     response=requests.get(newurl)
     #print("hello")
     soup1 = BeautifulSoup(response.text,'lxml')
-#    maintitle=soup1.select('.title_wrapper')
-    maintitle=[a.string for a in soup1.select('.title_wrapper h1')]
-    print(maintitle)
-
-    rating = [a.attrs.get('title') for a in soup1.select('.ratingValue strong')]
-    print(rating)
     releasedate= [a.string for a in soup1.select('.subtext a')]
     releasedate=releasedate[-1]
-    print(releasedate)
+#    print(releasedate)
+    rd.append(releasedate)
+    for i in bad_chars:
+        releasedate = releasedate.replace(i, '')
+    date_object = datetime.strptime(releasedate, '%d-%B-%Y %A').date()
+    print(type(date_object))
+    print(date_object)
+
+
+
     datetie = [a.string for a in soup1.select('.subtext time')]
-    print(datetie)
+#    print(datetie)
+
+
+    s=(str(datetie))
+    dur.append(s)
+#   print(dur)
     summarytext = [c.string for c in soup1.select('.summary_text')]
-    print(summarytext)
+#    print(summarytext)
+    y=str(summarytext)
+    desc.append(y)
+#    print(desc)
+    c+=1
+    print("wait i am getting data from site")
+    print(c)
+
+#    my=[releasedate,datetie,summarytext]
+#    mylist.append(my)
+#    print(mylist)
+for i in range(250):
+    newdata=[movietitle[i],ratings[i],rd[i],dur[i],desc[i]]
+    newlist.append(newdata)
+print(newlist)
+cur = conn.cursor()
+sql = ''' INSERT INTO MOVIES(TITLE,RATING,RELEASEDATE,DURATION,DESCRIPTION)
+              VALUES(?,?,?,?,?) '''
+for i in range(250):
+    cur.execute(sql,newlist[i])
 
 # Store each item into dictionary (data), then put those into a list (imdb)
-for index in range(0, len(movies)):
-    # Seperate movie into: 'place', 'title', 'year'
-    movie_string = movies[index].get_text()
-    movie = (' '.join(movie_string.split()).replace('.', ''))
-    movie_title = movie[len(str(index)) + 1:-7]
-    year = re.search('\((.*?)\)', movie_string).group(1)
-    place = movie[:len(str(index)) - (len(movie))]
-    data = {"movie_title": movie_title,
-            "year": year,
-            "place": place,
-            "star_cast": crew[index],
-            "rating": ratings[index],
-            "vote": votes[index],
-            "link": links[index]}
-    imdb.append(data)
 
-#for item in imdb:
- #   print(item['place'], '-', item['movie_title'], '(' + item['year'] + ') -', 'Starring:', item['star_cast'])
+conn.commit();
+print("completed")
